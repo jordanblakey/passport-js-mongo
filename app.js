14a519d8307f56342af58d4c4424144bee4a6cbd
Jordan <jordan@jordanblakey.com>
2017-10-06 07:51:14 -0500

    Login and authentication working

--- app.js
+++ app.js
@@ -1,47 +1,33 @@
-// Import Express & View Engine
 var express = require('express');
-var exphbs = require('express-handlebars');
-var expressValidator = require('express-validator');
-var session = require('express-session');
-require('popper.js')
-
-// Import Misc Server Utils
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
 var flash = require('connect-flash');
-
-// Import Passport
+var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-
-// Import MongoDB
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/loginapp', {
-  useMongoClient: true
-});
+
+mongoose.connect('mongodb://localhost/loginapp');
 var db = mongoose.connection;
 
-// Set API Routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 // Init App
 var app = express();
 
-// Init View Engine
+// View Engine
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({
-  defaultLayout: 'layout'
-}));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
 app.set('view engine', 'handlebars');
 
-// Init body-parser Middleware
+// BodyParser Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Set Static Folder
@@ -49,25 +35,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Express Session
 app.use(session({
-  secret: 'secret',
-  saveUninitialized: true,
-  resave: true
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
 }));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Express Validator
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
-    var namespace = param.split('.'),
-      root = namespace.shift(),
-      formParam = root;
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
 
-    while (namespace.length) {
+    while(namespace.length) {
       formParam += '[' + namespace.shift() + ']';
     }
     return {
-      param: formParam,
-      msg: msg,
-      value: value
+      param : formParam,
+      msg   : msg,
+      value : value
     };
   }
 }));
@@ -75,20 +65,23 @@ app.use(expressValidator({
 // Connect Flash
 app.use(flash());
 
-// Global Flash Vars
-app.use(function(req, res, next) {
+// Global Vars
+app.use(function (req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
   next();
 });
 
-// Intialize Routes
+
+
 app.use('/', routes);
 app.use('/users', users);
 
-// Set port and start Express
+// Set Port
 app.set('port', (process.env.PORT || 3000));
-app.listen(app.get('port'), function() {
-  console.log('Server started on port ' + app.get('port'));
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
 });
